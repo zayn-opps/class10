0631ff0cefd950513382dd9425f4d48ee08d0c26
zayn-opps <sojolsojol836@gmail.com>
2023-09-19 00:40:13 +0600

    java

--- index.js
+++ index.js
@@ -29,5 +29,35 @@ console.log("divishon",sub2);
 
 let  fullFORM=5000 +50-1000*2;
 let fullFORM1=5;
-let valu =fullFORM/fullFORM1
+let valu =fullFORM/fullFORM1;
 console.log(valu);
+//
+ let  roket=5000;
+  let cashOUT=300;
+  let total =roket-cashOUT
+  console.log("cashin",+total);
+  //jog fall
+  let mustafiz=1000;
+  let dipu=5000;
+  let jogaJOY=12000;
+  let riyad=550;
+  let alsome=0.55;
+  let intotal=mustafiz+dipu+jogaJOY+riyad+alsome;
+  console.log("valu",intotal);
+
+
+
+let sum1=50;
+let sum2=7;
+let persen=sum1 % sum2;
+console.log("multification",persen)
+//deceriment
+let joy_joga_gf=1 ;
+joy_joga_gf++;
+joy_joga_gf++;
+ let valu5 =+2;
+console.log("joy joga ",joy_joga_gf+valu5);
+
+
+
+
