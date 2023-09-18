fe7e9cbf3c414b0684ac328315fffe38f024a412
zayn-opps <sojolsojol836@gmail.com>
2023-09-18 22:41:48 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+console.log("helo world");
+
+var myFrinds="joy";
+console.log(myFrinds);
+
+let bikas=500;
+bikas++;
+console.log(bikas);
+
+let nogod=0.75;
+nogod++;
+let sub =nogod+55;
+console.log("sub",sub);
+ 
+let scoreBOARD=250;
+scoreBOARD--;
+
+let sub1=scoreBOARD-200;
+
+
+
+let sub2=scoreBOARD*2
+console.log("jogfall",sub1);
+
+
+console.log("divishon",sub2);
+
+
+
+let  fullFORM=5000 +50-1000*2;
+let fullFORM1=5;
+let valu =fullFORM/fullFORM1
+console.log(valu);
